5c13fbaf05cfa8e2d263604d89152e78d3a14980
Jack Reeves Eyre <jack.reeveseyre@gmail.com>
2025-04-18 21:37:39 -0400

    Adds observations to map figure 1.

--- config_semidiurnal.py
+++ config_semidiurnal.py
@@ -1,5 +1,6 @@
 DATA_DIR = '/home/jackre/Data/GCM/cfsm501/'
-PLOT_DIR = '/home/jackre/Documents/code/cpc_model/cfs-semidiurnal/plots/'
+PLOT_DIR = '/home/jackre/Documents/code/cpc_model/cfs-semidiurnal/plots/revisions_apr2025/'
+OBS_DIR = '/home/jackre/Data/ocean_obs/TAO/'
 
 GAS_CONSTANT = 287.05
 HEAT_CAPACITY = 1005.0
